3738d910e5915a2f23482149bc631fbff9f4e7de
ILYA EGOROV <egorovcf@gmail.com>
2021-08-13 14:37:39 +0300

    Second commit. Remarks

--- src/CoreMod/Main.java
+++ src/CoreMod/Main.java
@@ -18,9 +18,9 @@ public class Main {
 
         // Записываем сохранения в файлы
         try {
-            saveGame(savePath, gp1, "save_1.txt");
-            saveGame(savePath, gp2, "save_2.txt");
-            saveGame(savePath, gp3, "save_3.txt");
+            saveGame(savePath + "\\" + "save_1.dat", gp1);
+            saveGame(savePath + "\\" + "save_2.dat", gp2);
+            saveGame(savePath + "\\" + "save_3.dat", gp3);
         }
         catch (IOException ex){
             System.out.println("Возникла ошибка при записи сохранения");
@@ -28,9 +28,9 @@ public class Main {
 
         // Создаём список, содержащий в себе пути к сохранениям
         List<String> savesList = new ArrayList<>();
-        savesList.add("save_1.txt");
-        savesList.add("save_2.txt");
-        savesList.add("save_3.txt");
+        savesList.add(savePath + "\\" + "save_1.dat");
+        savesList.add(savePath + "\\" + "save_2.dat");
+        savesList.add(savePath + "\\" + "save_3.dat");
 
         try{
             zipFiles(savePath, savesList);
@@ -41,8 +41,8 @@ public class Main {
     }
 
 
-    public static void saveGame(String savingPath, GameProgress gp, String saveName) throws IOException {
-        FileOutputStream fos = new FileOutputStream(savingPath + "\\" + saveName);
+    public static void saveGame(String savingPath, GameProgress gp) throws IOException {
+        FileOutputStream fos = new FileOutputStream(savingPath);
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(gp);
         oos.close();
@@ -53,13 +53,15 @@ public class Main {
     public static void zipFiles(String pathForZip, List<String> savesList) throws IOException {
         // Создаём поток за запись архива, указываем место где архив будет лежать
         ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(pathForZip + "\\archive.zip"));
-        for (int i = 0; i < savesList.size(); i++) {
+        for (int i = 1; i <= savesList.size(); i++) {
             // Ентри - 1 единица записи. Указываем в текущем каталоге название файла
-            ZipEntry entry = new ZipEntry(savesList.get(i));
+            ZipEntry entry = new ZipEntry("save_" + i + ".dat");
+            System.out.println(entry.getName());
             // Уведомляем поток о новом элементе архива
             zout.putNextEntry(entry);
             // Создаём поток на чтение
-            FileInputStream fis = new FileInputStream(pathForZip + "\\" + savesList.get(i));
+            FileInputStream fis = new FileInputStream(savesList.get(i-1));
+            System.out.println(entry.getName());
             // Создаём буфер, в который будем считывать
             byte[] buffer = new byte[fis.available()];
             // Производим чтение из фалйа и записываем в буфер. По сути просто занимаемся переписыванием из одного места в другое
@@ -72,7 +74,7 @@ public class Main {
         zout.close();
 
         savesList.forEach(filename -> {
-            File saveForDelete = new File(pathForZip + "\\" + filename);
+            File saveForDelete = new File(filename);
             if (saveForDelete.delete())
                 System.out.println("Файл " + filename + " успешно удалён");
             else
